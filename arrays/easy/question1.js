ced5dc7b958ce95a3623eb07b25c32ed8ae89368
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-05-01 18:10:07 +0200

    :tada: first question first solution  :tada:

--- arrays/easy/question1.js
+++ arrays/easy/question1.js
@@ -6,3 +6,11 @@ Example:
 Input: 'n' = 5, 'arr' = [1, 2, 3, 4, 5]
 Output: 5
  */
+
+
+//First way
+function largest(arr){
+    arr.sort((a,b) => a - b)
+    return arr[arr.length-1]
+}
+console.log(largest([7,3,2,6,4]));
