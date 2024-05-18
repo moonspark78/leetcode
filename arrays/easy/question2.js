76c2c54ee717ec5aa5a86565a4a8305c495537c5
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-05-18 14:51:39 +0200

    :tada: question 2  start :tada:

--- arrays/easy/question2.js
+++ arrays/easy/question2.js
@@ -2,3 +2,8 @@
 Given an array Arr of size N, print the second largest distinct element from an array.
 If the second largest element doesn't exist then return -1.
 */
+
+
+function secondLargest(arr){
+    
+}
