0cb9e3fb3d01685ee19158696ea35b6028857c1c
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-05-21 01:33:10 +0200

    :tada: question 4 work  :tada:

--- arrays/easy/question4.js
+++ arrays/easy/question4.js
@@ -2,3 +2,15 @@
 Given an array nums containing n distinct numbers in the range [0, n], 
 return the only number in the range that is missing from the array.
 */
+
+function isMissing(arr){
+    const n = arr.length;
+    const expected = n * (n+1)/2
+    const actual = arr.reduce((acc, curr) => curr + acc, 0);
+
+    const missing = expected - actual;
+
+    return missing;
+}
+
+
